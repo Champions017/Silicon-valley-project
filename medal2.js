8f1e7db5113cd900853114baaeae6caa483567db
Champions017 <81851859+Champions017@users.noreply.github.com>
2021-07-10 13:26:37 +0530

    Update medal2.js

--- medal2.js
+++ medal2.js
@@ -3,6 +3,6 @@ function Lmedal2(){
         background(ABG)
         textFont("Montserrat")
         textAlign(CENTER)
-        fill("yellow")
+        fill("black")
         text("Press 'k' to start! \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nCongratulations! You just earned the plant growing badge! You need to collect only 3 more to unlock all of them!",width/2,30)
 }
